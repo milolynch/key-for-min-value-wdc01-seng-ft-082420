b26d9fca68fc60733e918efaf8c26039234ebb9d
beatific-chain-3778 <milobravman20@gmail.com>
2020-08-16 12:07:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,12 @@
 def key_for_min_value(name_hash)
   min = 1000
   name_hash.each do |key, value|
-
+    
     end
   end
+end
+hash = {key1: "value1", key2: "value2"}
+ 
+hash.each do |key, value|
+  puts "#{key}: #{value}"
 end
